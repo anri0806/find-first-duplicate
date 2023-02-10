75e5d56d56c8fd29859a94203b9e9bb26c5b04a9
Anri <anri0806@hotmail.co.jp>
2023-02-09 23:32:42 -0500

    gourth attempt ruby

--- ruby/find_first_duplicate.rb
+++ ruby/find_first_duplicate.rb
@@ -3,23 +3,20 @@ require 'set'
 def find_first_duplicate(arr)
   # type your code in here
 
-  #### SECOND ATTEMPT
-  #### Create empty set -->  #<Set: {}>  will look like this after addition
-  ####  #<Set: {1, 3, 4} >
-  #### Then iterate each value 
-  #### Return if set includes iterated value, if not, add value to set
+  newArr = Set.new
+  i = 0
 
-  uniques = Set.new
 
   arr.each do |value|
-    if uniques.include?(value)
-     return value 
+    if newArr.include?(value) 
+      return value
     else
-     uniques.add(value)
+      newArr << value
     end
   end
 
   -1
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -35,6 +32,27 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+
+
+
+  #### SECOND ATTEMPT
+  #### Create empty set -->  #<Set: {}>  will look like this after addition
+  ####  #<Set: {1, 3, 4} >
+  #### Then iterate each value 
+  #### Return if set includes iterated value, if not, add value to set
+
+  # uniques = Set.new
+
+  # arr.each do |value|
+  #   if uniques.include?(value)
+  #    return value 
+  #   else
+  #    uniques.add(value)
+  #   end
+  # end
+
+  # -1
+
 
 
 #### FIRST ATTEMPT
